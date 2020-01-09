82c83d697527c2ff67a81e32c73000d79e5b1deb
Nathan Wentworth <nwentworth95@gmail.com>
2020-01-08 19:35:12 -0500

    added basic dog controller, game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float TotalHeldTime { private get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+}
